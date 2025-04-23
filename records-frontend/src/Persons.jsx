fb9588a9438bd10d5474168076714c2b7ffbe986
P10r10 <asantiago@keemail.me>
2025-04-23 16:31:48 +0100

    edited Persons.jsx

--- records-frontend/src/Persons.jsx
+++ records-frontend/src/Persons.jsx
@@ -1,5 +1,24 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+
 function Persons() {
-    return (<>Persons</>);
+
+    const [persons, setPersons] = useState([]);
+    const PERSONS_URL = "http://127.0.0.1:8000/records/api/persons/";
+
+    useEffect(() => {
+        axios.get(PERSONS_URL).then(response => setPersons(response.data));
+    }, []);
+
+    return (
+        <div>
+            <h1>Persons</h1>
+            <ul>
+                {persons.map(person =>
+                    <li>{person.name} - {person.alias} - {person.date_of_birth}</li>)}
+            </ul>
+        </div>
+    );
 }
 
 export default Persons;
