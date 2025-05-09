93571771dc70cbb2189e1f3ee64d064fedb52164
P10r10 <asantiago@keemail.me>
2025-05-09 16:19:29 +0100

    criados botões de navegação

--- records-frontend/src/Components/MyNavbar/MyNavbar.jsx
+++ records-frontend/src/Components/MyNavbar/MyNavbar.jsx
@@ -3,10 +3,13 @@ import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
 import badgeImg from "./badge.jpg";
 import {useUserContext} from "../UserContext";
+import {useNavigate} from "react-router-dom";
+import "./navStyle.css";
 
 function MyNavbar() {
 
     const {username, userType, handleLogout} = useUserContext();
+    const navigate = useNavigate();
 
     return (
         <Navbar className="bg-body-tertiary">
@@ -19,14 +22,16 @@ function MyNavbar() {
                         height="30"
                         className="d-inline-block align-top"
                     />{" "}
-                    Força policial de investigação criminal</Navbar.Brand>
+                    Força policial de investigação criminal
+                    <Button variant="warning" onClick={() => navigate("/main")}>Menu</Button>
+                    <Button variant="warning" onClick={() => navigate("/persons")}>Pessoas</Button>
+                    <Button variant="warning" onClick={() => navigate("/casefiles")}>Processos</Button>
+                </Navbar.Brand>
                 <Navbar.Toggle/>
                 <Navbar.Collapse className="justify-content-end">
                     <Navbar.Text>
-                        Utilizador: <strong>{username}</strong> ({userType}) <
-                        Button
-                        variant="danger"
-                        onClick={handleLogout}>sair</Button>
+                        Utilizador: <strong>{username}</strong> ({userType})
+                        <Button variant="danger" onClick={handleLogout}>sair</Button>
                     </Navbar.Text>
                 </Navbar.Collapse>
             </Container>
