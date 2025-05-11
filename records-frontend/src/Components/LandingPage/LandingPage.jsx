f6f558fb7b2a5ec0a7bb2a6cdb0932af951fcb1c
P10R10 <asantiago@keemail.me>
2025-05-11 20:32:13 +0100

    ready to start working on Casefiles

--- records-frontend/src/Components/LandingPage/LandingPage.jsx
+++ records-frontend/src/Components/LandingPage/LandingPage.jsx
@@ -2,7 +2,7 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./landingStyle.css";
-import Popup from "../Popup";
+import Popup from "./Popup";
 import {useUserContext} from "../UserContext";
 
 function LandingPage() {
